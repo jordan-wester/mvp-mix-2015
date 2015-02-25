760a346c8dcc686ac50235eae0433b00940de1cd
Jordan Wester <jordan.r.wester@gmail.com>
2015-02-25 07:29:51 -0600

    Added ES6 example.js

---
+++ lessons-learned/examples/es6/modules.js
@@ -0,0 +1,12 @@
+
+// app/search.js
+export function search(searchCriteria) {
+  // perform search
+};
+
+
+
+// app/pagination.js
+import * as searchService from 'app/search';
+
+searchService.search(this.paginationData);
